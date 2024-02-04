348b17a0204e82c1793967382f49b88dbcfa423c
Tyson <tyson@horsewell.me>
2024-02-04 08:53:57 +0800

    app.py & climate_starter.ipynb
    connecting to the database

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,29 +1,38 @@
 # Import the dependencies.
+from pathlib import Path
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import automap_base
 
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
+database_path = Path("../Resources/hawaii.sqlite")
+engine = create_engine(f"sqlite:///{database_path}")
 
 # Declare a Base using `automap_base()`
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
